4f024bdceeb519ba2447a01acbda50b3f1a071c1
Yale Huang <yale@hwang.men>
2024-05-10 09:45:56 +0000

    Refactor FAQSummaryFixUI to improve code organization and add new features

--- ui/faq_summary_fix_ui.py
+++ ui/faq_summary_fix_ui.py
@@ -20,14 +20,36 @@ class FAQSummaryFixUI:
                     self.data_after = gr.Dataframe()
                     self.download_output = gr.File(label="Download Processed File")
 
-                    self.question_field = gr.Textbox(label="Question", value="Q")
-                    self.summary_field = gr.Textbox(label="Summary", value="A")
-                    self.q_and_a_field = gr.Textbox(label="Q&A", value="text")
                 with gr.Column():
                     self.data_before = gr.Dataframe()
                     with gr.Row():
                         self.file_input = gr.File(label="Upload your Excel file")
                         self.process_button = gr.Button("Process Data")
+            
+            with gr.Row():
+                with gr.Column():
+                    self.question_field = gr.Textbox(
+                        label="Question Field", value="Q")
+                    self.summary_field = gr.Textbox(
+                        label="Summary Field", value="A")
+                    self.q_and_a_field = gr.Textbox(
+                        label="Q&A Field", value="text")
+
+                with gr.Column():
+                    self.faq_id_output_field = gr.Textbox(
+                        label="FAQ ID Output Field", value="faq_id")
+                    self.faq_text_output_field = gr.Textbox(
+                        label="FAQ Text Output Field", value="faq_text")
+                    self.rdb_text_output_field = gr.Textbox(
+                        label="RDB Text Output Field", value="rdb_text")
+
+                with gr.Column():
+                    self.no_answer_pattern = gr.Textbox(
+                        label="No Answer Pattern", value="<NO ANSWER/>")
+                    self.no_answer_output_template = gr.Textbox(
+                        label="No Answer Output Template", value="For more information about `{question}`, please visit the following link: [{question}](faq:{faq_id}).")
+                    self.general_output_template = gr.Textbox(
+                        label="General Output Template", value="{output} For more details, please see the link [{question}](faq:{faq_id}).")
 
             self.file_input.change(
                 self._excel_file_uploaded,
@@ -36,7 +58,11 @@ class FAQSummaryFixUI:
             )
             self.process_button.click(
                 self._process_faq_data,
-                inputs=[self.data_before,self.question_field,self.summary_field,self.q_and_a_field],
+                inputs=[self.data_before,
+                        self.question_field, self.summary_field, self.q_and_a_field,
+                        self.faq_id_output_field, self.faq_text_output_field, self.rdb_text_output_field,
+                        self.no_answer_pattern, self.no_answer_output_template, self.general_output_template
+                        ],
                 outputs=[self.data_after]
             )
             self.data_after.change(
@@ -57,30 +83,45 @@ class FAQSummaryFixUI:
         characters)."""
         return str(ObjectId())
 
-    def _construct_faq_text(self, row, question, summary, q_and_a):
+    def _construct_faq_text(self, row, 
+                            question_field, summary_field, q_and_a_field,
+                            no_answer_pattern, no_answer_output_template, general_output_template
+                            ):
         """Construct FAQ text for each row based on given rules."""
-        question = row[question]
-        output = row[summary]
-        text = row[q_and_a]
+        question_field = row[question_field]
+        output = row[summary_field]
+        text = row[q_and_a_field]
         faq_id = self._generate_id()
 
-        if output == '<NO ANSWER/>':
-            answer = f"关于 `{question}`，详见链接 [{question}](faq:{faq_id})"
+        if output == no_answer_pattern:
+            answer = no_answer_output_template.format(question=question_field, faq_id=faq_id, output=output, text=text)
         else:
-            answer = f"{output} 详见链接 [{question}](faq:{faq_id})"
+            answer = general_output_template.format(question=question_field, faq_id=faq_id, output=output, text=text)
 
         if text.startswith('Q: '):
             text = text.split('A: ')[1]
 
-        return faq_id, f"Q: {question}\nA: {answer}", text
+        return faq_id, f"Q: {question_field}\nA: {answer}", text
 
-    def _process_faq_data(self, dataframe, question, summary, q_and_a):
+    def _process_faq_data(self, dataframe, question_field, summary_field, q_and_a_field,
+                          faq_id_output_field, faq_text_output_field, rdb_text_output_field,
+                          no_answer_pattern, no_answer_output_template, general_output_template):
         """Process the DataFrame to add new columns."""
         try:
             df = dataframe
-            df['faq_id'], df['faq_text'], df['rdb_text'] = zip(*df.apply(self._construct_faq_text,
-                                                    args=(question, summary, q_and_a),
-                                                    axis=1))
+            df[faq_id_output_field], df[faq_text_output_field], df[rdb_text_output_field] = \
+                zip(*df.apply(
+                    self._construct_faq_text,
+                    args=(
+                        question_field, 
+                        summary_field, 
+                        q_and_a_field,
+                        no_answer_pattern, 
+                        no_answer_output_template, 
+                        general_output_template
+                    ),
+                    axis=1
+                ))
             return df
         except Exception as e:
             raise gr.Error(f"Error processing data: {e}")
